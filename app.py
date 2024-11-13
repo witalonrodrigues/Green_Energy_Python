95eae43d2e0f92a83e3cbf4322dc588710c522e9
Witalon Rodrigues <162512211+witalonrodrigues@users.noreply.github.com>
2024-11-13 14:14:16 -0300

    feat - adicionando a função que exibe o menu principal

---
+++ app.py
@@ -0,0 +1,32 @@
+#Função Principal
+def main():
+    cadastrar_usuario()
+    while True:
+        print('Menu Energy Green (EaaS)')
+        print('1 - Consultar Pacote de Energia')
+        print('2 - Simulação de Consumo')
+        print('3 - Criar Relatório de Consumo e Emissão de Carbono')
+        print('4 - Relatório de Emissões de Carbono e Consumo')
+        print('5 - Gráfico de Consumo')
+        print('0 - Sair')
+
+        opcao = input("Escolha o que deseja visualizar: ")
+
+        if opcao == "1":
+            consultar_pacote()
+        elif opcao == "2":
+            simular_consumo()
+        elif opcao == "3":
+            registrar_dados_usuario()
+        elif opcao == "4":
+            gerar_relatorio()
+        elif opcao == '5':
+            graficos_consumo()
+        elif opcao == "0":
+            print("Encerrando o programa. Até mais!")
+            break
+        else:
+            print("OPÇÃO INVÁLIDA. Por favor, escolha uma opção válida.")
+
+if __name__ == "__main__":
+    main()
